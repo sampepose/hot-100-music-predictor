62955cf42aed8bef9555a9b4a79630cf5ec994c2
Zachary Myles Kovar <kovarzm@vmp747.vampire>
2016-04-27 17:29:35 -0500

    updating 0_setup for 2 hbase spotify tables

--- spotify_data/0_setup.py
+++ spotify_data/0_setup.py
@@ -2,19 +2,19 @@ import happybase
 import sys
 
 
-from variables import MACHINE, VUID, PAGE_TABLE, COLUMN_FAMILY, INDEX_TABLE
+from variables import MACHINE, VUID, PAGE_TABLE_SPOTIFY, COLUMN_FAMILY_SPOTIFY, COLUMN_SPOTIFY, PAGE_TABLE_SPOTIFY_TRACKS, TRACK_COLUMN_FAMILY, TRACK_COLUMN
 
 def setup():
     print "Creating..."
     connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
-    connection.create_table(PAGE_TABLE, {COLUMN_FAMILY: dict()})
-    # connection.create_table(INDEX_TABLE, {COLUMN_FAMILY: dict()})
+    connection.create_table(PAGE_TABLE_SPOTIFY, {COLUMN_FAMILY_SPOTIFY: dict()})
+    connection.create_table(PAGE_TABLE_SPOTIFY_TRACKS, {TRACK_COLUMN_FAMILY: dict()})
 
 def delete():
     print 'Deleting...'
     connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
-    connection.delete_table(PAGE_TABLE, True)
-    # connection.delete_table(INDEX_TABLE, True)
+    connection.delete_table(PAGE_TABLE_SPOTIFY, True)
+    connection.delete_table(PAGE_TABLE_SPOTIFY_TRACKS, True)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -22,12 +22,24 @@ if __name__ == '__main__':
             delete()
         elif sys.argv[1] == 'scan':
             connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
-            table = connection.table(PAGE_TABLE)
+            table = connection.table(PAGE_TABLE_SPOTIFY_TRACKS)
             i = 0
             for k, v in table.scan():
                 print i, k, v,
                 i += 1
                 if i > 10: break
+            
+            print "\n"
+            print "\n"
+            print "\n"
+
+            table = connection.table(PAGE_TABLE_SPOTIFY)
+            i = 0
+            for k, v in table.scan():
+                print i, k, v, 
+                i += 1
+                if i > 10: break
+
     else:
         setup()
 
