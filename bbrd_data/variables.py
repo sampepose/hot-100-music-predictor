503873d1b74211b43a504a0ecbddc2bacd97c0ea
Sam Pepose <sampepose@gmail.com>
2016-04-28 15:18:06 -0500

    Parse billboard date. unicode to ascii

---
+++ bbrd_data/variables.py
@@ -0,0 +1,18 @@
+VUID = 'kovarzm'
+MACHINE = 'vmp747'
+
+PAGE_TABLE_SPOTIFY = 'SpotifyTable'
+COLUMN_FAMILY_SPOTIFY = 'song'
+COLUMN_SPOTIFY = 'data'
+
+PAGE_TABLE_SPOTIFY_TRACKS = 'TrackID'
+TRACK_COLUMN_FAMILY = 'track'
+TRACK_COLUMN = 'data'
+
+BBRD_TABLE = 'Billboard'
+BBRD_COLUMN_FAMILY = 'date'
+BBRD_COLUMN = 'data'
+
+DEPENDENT_TABLE = 'Dependent'
+DEPENDENT_COLUMN_FAMILY = 'song'
+DEPENDENT_COLUMN = 'data'
