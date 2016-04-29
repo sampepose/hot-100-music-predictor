62f971a5c75db220659aae51e58323bdd78bcd70
Zachary Myles Kovar <kovarzm@vmp747.vampire>
2016-04-29 00:01:12 -0500

    updated script to put into both happybases

--- spotify_data/features/processFeatures.py
+++ spotify_data/features/processFeatures.py
@@ -3,6 +3,8 @@ import happybase
 
 from variables import MACHINE, VUID, TABLE_NAME_S, COLUMN_FAMILY_S, COLUMN_S, TABLE_NAME_BB, COLUMN_FAMILY_BB, COLUMN_BB
 
+# The JSON files aren't formatted correctly to be interpreted, so need to edit them in order to process
+
 with open('new_billboard.json', 'w') as bb_new:
     with open('billboardout.json', 'r') as data_file:
         for line in data_file:
@@ -28,11 +30,9 @@ with open('new_spotify.json', 'r') as sp:
 
 connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
 table = connection.table(TABLE_NAME_S)
-
 b = table.batch()
 
 tracktable = connection.table('TrackID')
-i = 0
 for elem in json_data:
     if 'error' not in elem:
         data = {}
@@ -45,11 +45,51 @@ for elem in json_data:
                 trackID = val[13:-2]
                 if elem['id'] == trackID:
                     key = k
-        #print(key, {COLUMN_FAMILY_S+ ":" + COLUMN_S: json.dumps(data)})
         b.put(key, {COLUMN_FAMILY_S + ":" + COLUMN_S: json.dumps(data)})
 b.send()
 
-#with open('new_spotify.json', 'r') as sp:
-#    json_data = json.load(sp)
+with open('new_billboard.json', 'r') as bb:
+    json_data = json.load(bb)
+
+table = connection.table(TABLE_NAME_BB)
+b = table.batch()
+
+i = 0
+tups = list()
+with open('billboardOnly.txt','r') as bbfile:
+    for line in bbfile:
+        l = line.split(',')
+        if len(l) == 3: #ignore songs/artists with a comma in it, makes it hard to process lines
+            tups.append((l[0][2:-1],l[1][2:-1],l[2][3:-3]))
+
+for elem in json_data:
+    if 'error' not in elem:
+        data = {}
+        for k,v in elem.items():
+            data[k] = v
+
+        for i in tups:
+            trackID = i[2][31:]
+            if elem['id'] == trackID:
+                key = i[0] + '_' + i[1]
+                
+        b.put(key, {COLUMN_FAMILY_BB + ":" + COLUMN_BB: json.dumps(data)})
+b.send()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
