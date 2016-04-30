63f0e79c4c63ff392e8f50e33e1c2e958473a67c
John <john.l.lally.1@vanderbilt.edu>
2016-04-29 19:14:16 -0500

    Processing Discogs Data

--- discogs_data/getDiscogs.py
+++ discogs_data/getDiscogs.py
@@ -1,10 +1,14 @@
 import discogs_client
 import happybase
 import json
+import unicodedata
 
 from variables import DEPENDENT_TABLE, DEPENDENT_COLUMN_FAMILY, DCOG_TABLE, DCOG_COLUMN_FAMILY, DCOG_COLUMN, MACHINE, VUID
 
 d = discogs_client.Client('ExampleApplication/0.1', user_token="RAzmbwmoidsSvCfkfKKnWoRpYnLKGsYMVwsXvhrZ")
+
+def convert(uni):
+    return unicodedata.normalize('NFKD', uni).encode('ascii', 'ignore')
 
 connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
 dcog_table = connection.table(DCOG_TABLE)
@@ -14,15 +18,19 @@ count = 0
 artist_fail = 0
 style_fail = 0
 
+# For every entry in the dependent dataset, get associated discog data
 for key,data in dpd_table.scan():
+
     dpd_data = json.loads(data.itervalues().next())
     title = dpd_data['title']
     artist = dpd_data['artist']
-
+    count += 1
+    print (key)
+    
     # Try to search for artist
     try:
         artist_search = d.search(artist, type = 'artist')
-        alias = [art.name for art in  artist_search[0].aliases]
+        alias = [convert(art.name) for art in  artist_search[0].aliases]
     except:
         artist_fail += 1
         alias = None
@@ -37,15 +45,13 @@ for key,data in dpd_table.scan():
         styles = None
         genres = None
 
-    # If all succeeded
-    if(alias and styles and genres):
-        count +=1
-
+    # Put into HBASE
     dcog_data = {'alias':alias, 'styles':styles,'genres':genres}
     dcog_table.put(key, {DCOG_COLUMN_FAMILY +':' + DCOG_COLUMN : json.dumps(dcog_data)})
 
 
+# Statistics about failures 
+print("Success Count:  " + str(count))
+print("Artist Failure Count:  " + str(artist_fail))
+print("Sytle Failure Count:  " + str(style_fail))
 
-print("Success Count:  " + count)
-print("Artist Failure Count:  " + artist_fail)
-print("Sytle Failure Count:  " + style_fail)
