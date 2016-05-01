a8c72032c29e971fa9f755bf61d5e7d8493b8ca6
Zachary Myles Kovar <kovarzm@vmp747.vampire>
2016-05-01 00:18:12 -0500

    minor updates

--- spotify_data/scripts/createSpotifyDatabase.py
+++ spotify_data/scripts/createSpotifyDatabase.py
@@ -1,5 +1,5 @@
 #Python script to add the CSV data into a DB
-
+import unicodedata
 import happybase
 import json
 import sys
@@ -9,10 +9,15 @@ import os
 
 from variables import MACHINE, VUID, SPOTIFY_TABLE, SPOTIFY_COLF, SPOTIFY_COL
 
+def convert(uni):
+    return unicodedata.normalize('NFKD', uni).encode('ascii', 'ignore')
+
 connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
 table = connection.table(SPOTIFY_TABLE)
 
 b = table.batch()
+
+songs = dict()
 
 for i in os.listdir("./spotify_data/data/"):
     if (i.find('.csv') != -1):   #ignore non csv files
@@ -20,15 +25,16 @@ for i in os.listdir("./spotify_data/data/"):
         with open(fname) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                
+            
                 data = {}
-                data['title'] = row['Track Name'].lower()
+                data['title'] = convert(unicode(row['Track Name'], "utf-8")).lower()
                 #title = re.sub(r'\(feat.*\)', '', title).strip()
-                data['artist'] = row['Artist'].lower()
+                data['artist'] = convert(unicode(row['Artist'], "utf-8")).lower()
                 key = data['title'] + '_' + data['artist']
 
+                if key not in songs:
+                    songs[key] = data
+                
                 b.put(key, {SPOTIFY_COLF + ':' + SPOTIFY_COL : json.dumps(data)})
             
-            b.send()
-            print "All data put in happybase: ",SPOTIFY_TABLE
-
+b.send()
