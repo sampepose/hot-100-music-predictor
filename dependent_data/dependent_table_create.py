6476b88df4f45ad40c88277613176d880ec48fa8
Sam Pepose <sampepose@gmail.com>
2016-04-30 23:20:48 -0500

    added edit distance for matching. added more hot 100 songs to join table

--- dependent_data/dependent_table_create.py
+++ dependent_data/dependent_table_create.py
@@ -1,50 +1,42 @@
 import happybase
 import json
-from datetime import datetime
 from collections import defaultdict
+import editdistance
+import re
+from datetime import datetime
 
-from variables import MACHINE, VUID, PAGE_TABLE_SPOTIFY, BBRD_TABLE, DEPENDENT_TABLE, DEPENDENT_COLUMN_FAMILY, DEPENDENT_COLUMN
 
-def hbaseAccelerationParser(table): #create UDF to format data
-    finalTable = []
-    for key, data in table.scan(): #don't need the key in my case
-        print(key)
-        print("\n")
-        finalTable.append(data)
-    return finalTable
+from variables import MACHINE, VUID, SPOTIFY_TABLE, BBRD_TABLE, DEPENDENT_TABLE, DEPENDENT_COLUMN_FAMILY, DEPENDENT_COLUMN
 
-def isLabel(tag):
-    if tag.previous_element is not None and tag.previous_element.previous_sibling is not None:
-        th = tag.previous_element.previous_sibling
-        a = th.next_element
-        return a['title'] == 'Record label'
-    return False
+def convert(uni):
+    return unicodedata.normalize('NFKD', uni).encode('ascii', 'ignore')
 
 def index():
     connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
-    spotify_table = connection.table(PAGE_TABLE_SPOTIFY)
+    spotify_table = connection.table(SPOTIFY_TABLE)
     bbrd_table = connection.table(BBRD_TABLE)
    
+    both_count = 0
     song_artists = {}
 
     spotify_count = 0
     spotify_seen_songs = []
     for key, data in spotify_table.scan():
-        key = key.lower()
+        key = re.sub(r'\(feat.*\)', '', key).strip().lower()
         if key not in spotify_seen_songs:
             spotify_count += 1
             spotify_seen_songs.append(key)
         data = json.loads(data.itervalues().next())
         if key not in song_artists:
-            song_artists[key] = {'title': data['song'], 'artist': data['artist'], 'isHot': False}
-
-    spotify_start_date = datetime.strptime('2015-04-25', '%Y-%m-%d')
+            song_artists[key] = {'title': data['title'], 'artist': data['artist'], 'isHot': False}
     
+    spotify_start_date = datetime.strptime('2014-01-01', '%Y-%m-%d')
+
     bbrd_artist_count_map = defaultdict(int)
     bbrd_count = 0
     bbrd_seen_songs = []
     for key, data in bbrd_table.scan():
-        key = key.lower()
+        key = re.sub(r'\(feat.*\)', '', key).strip().lower()
         data = json.loads(data.itervalues().next())
         date = datetime.strptime(data['date'], '%Y-%m-%d')
         bbrd_artist_count_map[data['artist']] += 1
@@ -54,8 +46,22 @@ def index():
             bbrd_count += 1
             bbrd_seen_songs.append(key)
         if key not in song_artists:
-            song_artists[key] = {'title': data['title'], 'artist': data['artist'], 'isHot': True}
+            foundEditDistance = False
+            for key2 in song_artists.keys():
+                if len(key) > 5 and editdistance.eval(key,key2) <= 2:
+                    bbrd_count -= 1
+                    spotify_count -=1
+                    both_count +=1
+                    print "Edit distance found: " + key + "\t" + key2
+                    song_artists[key2]['isHot'] = True
+                    foundEditDistance = True
+                    continue
+            if not foundEditDistance:
+                song_artists[key] = {'title': data['title'], 'artist': data['artist'], 'isHot': True}
         else:
+            bbrd_count -= 1
+            spotify_count -= 1
+            both_count += 1
             song_artists[key]['isHot'] = True
 
     depTable = connection.table(DEPENDENT_TABLE)
@@ -63,15 +69,15 @@ def index():
 
     for sa in song_artists:
         data = song_artists[sa]
-        data['ArtistPopularity'] = bbrd_artist_count_map[data['artist']]
+        data['ArtistPopularity'] = max(0, bbrd_artist_count_map[data['artist']] - 1)
         key = DEPENDENT_COLUMN_FAMILY + ":" + DEPENDENT_COLUMN
         b.put(sa, {key: json.dumps(data)})
     b.send()
 
     print("Spotify count", spotify_count)
     print("BBRD count", bbrd_count)
+    print("Overlap", both_count)
     print("Total count", len(song_artists))
-    print("Overlap", (bbrd_count + spotify_count) - len(song_artists))
 
 if __name__ == '__main__':
     index()
