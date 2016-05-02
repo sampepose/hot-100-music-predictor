c0e1c53fa114b4d45892b0ad49e9afef13754e65
Sam Pepose <sampepose@gmail.com>
2016-05-01 22:05:05 -0500

    Added custom threshold for accuracy

--- machinelearning/predict.py
+++ machinelearning/predict.py
@@ -13,22 +13,42 @@ from twitter.getFeatures import getTwitterFeatures
 from spotify_data.machinelearning.getSpotifyFeatures import getSpotifyFeatures
 from discogs_data.machinelearning.getFeatures import getDiscogsFeatures
 
+def classify(YPredProba, thresh):
+    labels = []
+    for yp in YPredProba:
+        if yp > thresh:
+            labels.append(1)
+        else:
+            labels.append(0)
+    return labels
+
+def accuracy(YTrue, YPred):
+    correct, count = 0, 0
+    for (yt, yp) in zip(YTrue, YPred):
+        count += 1
+        if yt == yp:
+            correct += 1
+    return float(correct) / count
+
 def test_classifier(clf, X, Y):
     folds = StratifiedKFold(Y, 5)
     aucs = []
     accs = []
+
+    #for x in np.arange(0, 1, 0.05):
     for train, test in folds:
-        # Sizes
-        # print X[train].shape, Y[train].shape
-        # print X[test].shape, len(prediction)
+            #aucs = []
+            #accs = []
 
         clf.fit(X[train], Y[train])
         prediction = clf.predict(X[test])
         prediction_proba = clf.predict_proba(X[test])
-        accs.append(accuracy_score(Y[test], prediction))
+
+        pred = classify(prediction_proba[:, 1], 0.6) # Value found via cross validation
+        accs.append(accuracy(Y[test], pred))
         aucs.append(roc_auc_score(Y[test], prediction_proba[:, 1]))
     print "Acc:\t", clf.__class__.__name__, "\t", np.mean(accs)
-    #print "AUC:\t", clf.__class__.__name__, "\t", np.mean(aucs)
+    print "AUC:\t", clf.__class__.__name__, "\t", np.mean(aucs)
 
 
 def main():
@@ -39,7 +59,7 @@ def main():
 
     XIdxs = [0, 1, 2, 3]
     XNames = ["Dep", "Twitter", "Spotify", "Discogs"]
-    Xs = [depfeatures, twitter_features, spotify_features,discogs_features]
+    Xs = [depfeatures, twitter_features, spotify_features, discogs_features]
     Y = labels
 
     # Test each feature set combination
@@ -55,14 +75,14 @@ def main():
 
             XSubset = [Xs[i] for i in subset]
             X = np.hstack(tuple(XSubset))
-        
+       
             clf = linear_model.SGDClassifier(loss='log')
             test_classifier(clf, X, Y)
 
             clf = GaussianNB()
             test_classifier(clf, X, Y)
-
-            clf = RandomForestClassifier(n_estimators=10, max_depth=10)
+            
+            clf = RandomForestClassifier(n_estimators=100, max_depth=100)
             test_classifier(clf, X, Y)
 
             print "\n"
