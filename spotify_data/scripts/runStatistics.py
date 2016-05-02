a6cd7e50951d42e9fd74f2552e7a36ca5133be12
Zachary Myles Kovar <kovarzm@vmp747.vampire>
2016-05-02 01:29:29 -0500

    file to pull stats on most times on top200 chart

---
+++ spotify_data/scripts/runStatistics.py
@@ -0,0 +1,52 @@
+import unicodedata
+import happybase
+import json
+import sys
+import csv
+import re
+import os
+import operator
+
+from variables import MACHINE, VUID, SPOTIFY_COUNTS, SPOTIFY_COLF, SPOTIFY_COL
+
+
+def getTopX(l,x,y):
+    indx = 0
+    d = dict() #return this
+    for i in l:
+        if indx >= x:
+            d[i[0]] = i[1]
+        if indx == y:
+            return sorted(d.items(), key=lambda x: x[1], reverse=True)
+        indx += 1
+    return sorted(d.items(), key=lambda x: x[1], reverse=True)
+
+connection = happybase.Connection(MACHINE + '.vampire', table_prefix=VUID)
+table = connection.table(SPOTIFY_COUNTS)
+
+plays = dict()
+days = dict()
+
+for key, val in table.scan():
+    
+    data = json.loads(val.itervalues().next())
+    plays[key] = float(data['plays'])
+    days[key] = data['days']
+
+
+
+plays = sorted(plays.items(), key=lambda x: x[1], reverse=True)
+days = sorted(days.items(), key=lambda x: x[1], reverse=True)
+
+top_days = getTopX(days,0,5)
+top_plays = getTopX(plays,0,5)
+
+print "Most days:"
+for i in top_days:
+    print i
+
+print "Most plays:"
+for i in top_plays:
+    print i
+
+
