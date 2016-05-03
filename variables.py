dbca6bd2a5996aadfcb77a8fb130da3d4e756951
Zachary Myles Kovar <kovarzm@vmp747.vampire>
2016-05-02 20:50:54 -0500

    removing old table names

--- variables.py
+++ variables.py
@@ -12,16 +12,6 @@ SPOTIFY_FEATURES = 'FeaturesFromSpotify'
 FEATURES_COLF = 'features'
 FEATURES_COL = 'data'
 
-#old
-PAGE_TABLE_SPOTIFY = 'SpotifyTable'
-COLUMN_FAMILY_SPOTIFY = 'song'
-COLUMN_SPOTIFY = 'data'
-
-#old
-PAGE_TABLE_SPOTIFY_TRACKS = 'TrackID'
-TRACK_COLUMN_FAMILY = 'track'
-TRACK_COLUMN = 'data'
-
 BBRD_TABLE = 'Billboard'
 BBRD_COLUMN_FAMILY = 'date'
 BBRD_COLUMN = 'data'
@@ -45,19 +35,6 @@ DCOG_F_COLUMN = 'data'
 DCOG_F2_TABLE = "DiscogsFeatures2"
 DCOG_F2_COLUMN_FAMILY = "features"
 DCOG_F2_COLUMN = 'data'
-
-#old
-TABLE_NAME_S = 'SpotifyFeatures'
-COLUMN_FAMILY_S = 'song'
-COLUMN_S = 'data'
-
-#old
-TABLE_S_FIXED = 'SpotifyFeaturesFixed'
-
-#old (i think)
-TABLE_NAME_BB = 'BillboardFeatures'
-COLUMN_FAMILY_BB = 'song'
-COLUMN_BB = 'data'
 
 TWITTER_TABLE = 'Twitter'
 TWITTER_COLUMN_FAMILY = 'counts'
